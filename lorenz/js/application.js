21af9a876b59f2e2c566d8c1062e4f32a6b53d24
Ali Can Demiralp <demiralpali@gmail.com>
2020-03-13 22:15:09 +0100

    Update application.js

--- lorenz/js/application.js
+++ lorenz/js/application.js
@@ -11,7 +11,7 @@ class Application
     this.controls = new TrackballControls(this.camera, this.renderer.domElement);
     this.renderer.setSize                (window.innerWidth, window.innerHeight);
     this.renderer.setPixelRatio          (window.devicePixelRatio);
-    this.camera  .position.set           (0, 0, -100);
+    this.camera  .position.set           (0, 0, -10);
     this.controls.update                 ();
 
     window.addEventListener('resize', function() 
